b1acfcbd8f733f4358bbfb12eb44b1f03e96ca95
Brian J Brennan <brianloveswords@gmail.com>
2013-11-17 10:16:35 -0500

    Fix input length count.

--- static/post-status.js
+++ static/post-status.js
@@ -4,9 +4,14 @@ const $$ = document.querySelectorAll.bind(document)
 const $input = $('#status')
 const $count = $('#character-count')
 
+const placeholder = 'xxxxxxxxxxxxxxxxxxxxx' // t.co links are 20 chars
+
 function updateCount() {
   const MAXLEN = 140
-  const remaining = MAXLEN - $input.value.length
+  const inputText = $input.value
+    .replace(/http:\/\/\S*/, placeholder)
+    .replace(/https:\/\/\S*/, placeholder + 'x')
+  const remaining = MAXLEN - inputText.length
   $count.textContent = remaining
 }
 
